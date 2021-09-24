0717c2a87ac3c8baf55cf9df7d9e35e19c1ad92f
kh-hathout <info@kh-hathout.org>
2021-09-24 13:12:00 +0300

    2021092404Local

---
+++ stock_picking_cancel_cs/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Cancel Delivery Order & Reset to Draft',
+    'version' : '1.0',
+    'author':'Craftsync Technologies',
+    'category': 'Stock',
+    'maintainer': 'Craftsync Technologies',
+    'summary': """Enable auto cancel transfered delivery or cancel stock picking. Auto Cancel Delivery Order and Auto Cancel Receivable products.""",
+    'website': 'https://www.craftsync.com/',
+    'license': 'OPL-1',
+    'support':'info@craftsync.com',
+    'depends' : ['account_cancel','sale_management','purchase','sale_stock'],
+    'data': [
+        'views/stock_picking.xml',
+        'views/res_config_settings_views.xml',
+        'views/view_sale_order.xml',
+        'views/view_purchase_order.xml',
+        'wizard/view_cancel_delivery_wizard.xml',
+
+    ],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/main_screen.png'],
+    'price': 39.00,
+    'currency': 'EUR',
+
+}
